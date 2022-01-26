4835e657d8246c1e246ede3ec829d967b41c00e9
BernhardBaumrock <office@baumrock.com>
2022-01-26 14:52:59 +0100

    fix non-quote ids issue

--- TextformatterRockHeadlineIDs.module.php
+++ TextformatterRockHeadlineIDs.module.php
@@ -14,7 +14,7 @@ class TextformatterRockHeadlineIDs extends Textformatter {
   public static function getModuleInfo() {
     return [
       'title' => 'RockHeadlineIDs',
-      'version' => '1.0.0',
+      'version' => '1.0.1',
       'summary' => 'Demo Textformatter',
     ];
   }
@@ -27,10 +27,17 @@ class TextformatterRockHeadlineIDs extends Textformatter {
       $markup = $matches[0];
       $starttag = $matches[1];
       $endtag = $matches[4];
-      preg_match("/(id=['|\"]?(\S*?)['|\"]?[\s|>])/", $markup, $findID);
+      preg_match("/(id=['\"]?(\S+?)['\"]+)|(id=(\S+))>?/", $starttag, $findID);
       if($dump) db($findID, 'findID');
       if(count($findID)===3) {
         $rawid = $findID[2];
+        $id = $this->getID($rawid);
+        while(in_array($id, $ids)) $id.="-";
+        $ids[] = $id;
+        return str_replace($rawid, $id, $markup);
+      }
+      elseif(count($findID)===5) {
+        $rawid = $findID[4];
         $id = $this->getID($rawid);
         while(in_array($id, $ids)) $id.="-";
         $ids[] = $id;
@@ -64,12 +71,16 @@ class TextformatterRockHeadlineIDs extends Textformatter {
    * $format->test();
    */
   public function test() {
-    $str = "<h1 class='foobar'><span id='test'>test das ist jö</span></h1><h2 id='test2' class='foo'>test2</h2>
+    $str = "
+      <h1 class='foobar'><span id='test'>test das ist jö</span></h1><h2 id='test2' class='foo'>test2</h2>
       <h2 id='same-id-test'>same id test1</h2>
       <h2 id='same-id-test'>same id test2</h2>
-      <h3>noch ein test</h3>\n<h4>foo bar</h4>
+      <h3>noch ein test</h3>
+      <h4>foo bar</h4>
       <h3>same id</h3>
-      <h4>same id</h4>";
+      <h4>same id</h4>
+      <h5 id=foo>test without quotes</h5>
+      <h5 id=foo class=bar>test without quotes</h5>";
     $this->format($str, true);
     db($str, "result");
   }
